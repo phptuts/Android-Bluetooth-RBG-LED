719fe505b140ee844051074e46c0711a477612d0
phptuts <noah.php@gmail.com>
2018-07-26 10:04:50 -0700

    Got the some of the bugs out and clean up a lot of code replace default icon with cool home grown icon.

--- app/src/main/java/net/noahglaser/bluetoothrxrgbled/MainActivity.kt
+++ app/src/main/java/net/noahglaser/bluetoothrxrgbled/MainActivity.kt
@@ -9,13 +9,20 @@ import io.vrinda.kotlinpermissions.PermissionsActivity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothSocket
 import android.graphics.Color
+import android.view.Menu
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import android.view.MenuItem
 
 
 class MainActivity : PermissionsActivity()  {
 
-    var canUseBluetooth = false
+    val STATUS_CONNECTTING = "Status: Connecting..."
+
+    val STATUS_DISCONNECTED = "Status: Disconnected"
+
+    val STATUS_CONNECTTED = "Status: Connected"
 
     var mBluetoothAdapter: BluetoothAdapter? = null
 
@@ -24,33 +31,28 @@ class MainActivity : PermissionsActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sb_blue.isEnabled = false
-        sb_green.isEnabled = false
-        sb_red.isEnabled = false
-
+        tv_status.text = STATUS_CONNECTTING
+        progressBarsEnabled(false)
 
         requestPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, object : PermissionCallBack {
             override fun permissionGranted() {
                 super.permissionGranted()
-                setupBluetooth()
-                Log.d("Call permissions", "Granted")
-                if (!canUseBluetooth) {
+                if (!setupBluetooth()) {
                     Toast.makeText(applicationContext, "Please make sure your bluetooth is connected and paired.", Toast.LENGTH_LONG).show()
                     return
                 }
-                writeBluetooth()
+                updateColor()
             }
 
             override fun permissionDenied() {
                 super.permissionDenied()
                 Toast.makeText(applicationContext, "Can't use bluetooth with location permission, ask google?", Toast.LENGTH_LONG).show()
-                Log.d("Call permissions", "Denied")
             }
         })
 
         val listener = object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                writeBluetooth()
+                updateColor()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -58,7 +60,7 @@ class MainActivity : PermissionsActivity()  {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
-        };
+        }
 
         sb_blue.setOnSeekBarChangeListener(listener)
         sb_green.setOnSeekBarChangeListener(listener)
@@ -66,35 +68,83 @@ class MainActivity : PermissionsActivity()  {
 
     }
 
-    fun setupBluetooth() {
+    /**
+     * This function tries to open a connection between the phone and the bluetooth
+     * It returns true if successful
+     */
+    fun setupBluetooth(): Boolean {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         mBluetoothAdapter.let {
             val device = mBluetoothAdapter?.bondedDevices?.first()
             val uuid = device?.uuids?.first()?.uuid
             socket = device?.createRfcommSocketToServiceRecord(uuid)
-            socket?.connect()
-
-            if (socket != null) {
-                sb_red.isEnabled = true
-                sb_blue.isEnabled = true
-                sb_green.isEnabled = true
-                canUseBluetooth = true
+            return try {
+                socket?.connect()
+                progressBarsEnabled(true)
+                tv_status.text = STATUS_CONNECTTED
+                true
+            } catch (e: IOException) {
+                Log.d("setupBluetooth", e.message)
+                progressBarsEnabled(false)
+                tv_status.text = STATUS_DISCONNECTED
+                false
             }
         }
     }
 
-    fun writeBluetooth() {
+    /**
+     * Sends a color message to the bluetooth device
+     * Update the background color and the color text
+     * If it can't send a message it tries to set up a connection again
+     */
+    fun updateColor() {
+
         val message = "${sb_red.progress}-${sb_green.progress}-${sb_blue.progress}|"
-        if (socket?.isConnected == false) {
-            Toast.makeText(applicationContext, "Bluetooth was disconnect, please connect and try again.", Toast.LENGTH_LONG).show()
-            return
+        val hexColor = "#" + sb_red.progress.getHexColor() +
+                sb_green.progress.getHexColor() +
+                sb_blue.progress.getHexColor()
+        try {
+            socket?.outputStream?.write(message.toByteArray())
+            main_layout.setBackgroundColor(Color.parseColor(hexColor))
+            tv_hex_color.text = "Hex Color: $hexColor"
+        } catch (e: IOException) {
+            tv_status.text = STATUS_CONNECTTING
+            Toast.makeText(applicationContext, "Bluetooth Disconnected, Trying to re establish connection", Toast.LENGTH_LONG).show()
+            setupBluetooth()
         }
-        var hexColor = "#" + sb_red.progress.getHexColor() +
-                        sb_green.progress.getHexColor() +
-                        sb_blue.progress.getHexColor()
-        socket?.outputStream?.write(message.toByteArray())
-        main_layout.setBackgroundColor(Color.parseColor(hexColor))
-        tv_hex_color.text = "Hex Color: $hexColor"
+    }
+
+    /**
+     * In ables and disables the progress bar
+     */
+    private fun progressBarsEnabled(enabled: Boolean) {
+        sb_blue.isEnabled = enabled
+        sb_green.isEnabled = enabled
+        sb_red.isEnabled = enabled
+    }
+
+    /**
+     * Event listener for when the Menu Option is clicked
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_refresh -> {
+                Toast.makeText(applicationContext, "Refreshing Bluetooth", Toast.LENGTH_SHORT).show()
+                if (setupBluetooth()) updateColor()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * Boiler plate code that attaches the menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_layout, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
 
